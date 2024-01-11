5624a8939e1175f860892e65c4813b0eef911eab
Alfarezi08 <rahbrayy@gmail.com>
2024-01-11 09:21:26 +0700

    Mengunggah file UAS

---
+++ kirimWA.py
@@ -0,0 +1,11 @@
+import pywhatkit
+import pyautogui
+import time
+
+pywhatkit.sendwhatmsg("+6281266398662","Halo saya alfarezi", 5,0)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.press('enter')
+
+
